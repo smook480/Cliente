eaf0cf6b223702fb3d5493b9965573375e025d26
smk <ricardo.g.g.r@hotmail.com>
2022-12-22 18:09:28 -0600

    Se modificaron estilos se agrego Jquey en el navbar, se agrego icono de la marcar en el navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { faComputer } from '@fortawesome/free-solid-svg-icons';
+
 declare var $: any;
+declare var jQuery: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit{
+  faComputer=faComputer;
   constructor(){}
   ngOnInit()  {
-        $(document).ready(() =>{
-
-      // hide .navbar first
-      $(".navbar").hide();
+    $(document).ready(function(){
+      var $nav = $('.nav');//Caching element
+      // hide .navbar first - you can also do this in css .nav{display:none;}
+      $nav.hide();
   
       // fade in .navbar
-      $( () => {
-          $(window).scroll( () => {
-  
-                   // set distance user needs to scroll before we start fadeIn
-              if ($(this).scrollTop() > 100) {
-                  $('.navbar').fadeIn();
+      $(function () {
+          $(window).scroll(function (this:any) {
+              // set distance user needs to scroll before we start fadeIn
+              if ($(this).scrollTop() > 100) { //For dynamic effect use $nav.height() instead of '100'
+                  $nav.fadeIn();
               } else {
-                  $('.navbar').fadeOut();
+                  $nav.fadeOut();
               }
           });
       });
   
   });
-  }
 }
 
-
+}
